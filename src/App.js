fc8c3732c2b2d5fb225c854d6b271629a557718e
Ananya Tripathy <tripathykhushi2002@gmail.com>
2023-03-06 13:47:24 +0530

    initailze

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.png';
+import './App.css';
+import bg from "./logo.png"
+import animation from "./animation.gif"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">      
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
